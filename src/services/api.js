f3e2ff858f4950805167c2ab268e9d22f741eeb9
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 19:51:43 -0300

    refactor: remove readers api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const ApiService = axios.create({ 
-  baseURL: 'https://mynotes-back.herokuapp.com/',
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  },
+  baseURL: 'https://mynotes-back.herokuapp.com/'
 })
 
 export default ApiService
