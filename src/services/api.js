6c2bbb540fcc42a419a02df1e9070c402e971ce8
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 20:49:27 -0300

    remove headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const ApiService = axios.create({ 
-  baseURL: 'https://mynotes-back.herokuapp.com/',
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  }
+  baseURL: 'https://mynotes-back.herokuapp.com/'
 })
 
 export default ApiService
