d11a922e5fb1f0c4b09053d6854e5320e5190740
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 19:29:25 -0300

    fix: add Access-Control-Allow-Origin on api headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
-const ApiService = axios.create({ baseURL: 'https://mynotes-back.herokuapp.com/' })
+const ApiService = axios.create({ 
+  baseURL: 'https://mynotes-back.herokuapp.com/',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+})
 
 export default ApiService
