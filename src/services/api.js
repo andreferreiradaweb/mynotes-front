d5a5316fc95135f93803a5ccb63d55c6570a4035
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 20:04:01 -0300

    refactor: add headers api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const ApiService = axios.create({ 
-  baseURL: 'https://mynotes-back.herokuapp.com/'
+  baseURL: 'https://mynotes-back.herokuapp.com/',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 
 export default ApiService
