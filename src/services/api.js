6cc2b27026c348f2d1176c57121dd44d8d2654ff
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 13:55:58 -0300

    update gif

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const ApiService = axios.create({ baseURL: 'http://localhost:3001' })
+const ApiService = axios.create({ baseURL: 'https://mynotes-back.herokuapp.com/' })
 
 export default ApiService
